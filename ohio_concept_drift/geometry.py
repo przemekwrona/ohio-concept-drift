acd88ed07829fe73ac79f523d2e7facdabee8166
przemekwrona <przemekwrona25@gmail.com>
2025-06-25 22:51:52 +0200

    feat: plot ohio with cities

--- ohio_concept_drift/geometry.py
+++ ohio_concept_drift/geometry.py
@@ -6,28 +6,6 @@ import sys
 OHIO_GEOMETRY = 'resources/ohio.geojson'
 
 
-def load_ohio_geojson():
-    # Assuming you have a GeoJSON file named 'my_data.geojson'
-    try:
-        with open(OHIO_GEOMETRY, 'r', encoding='utf-8') as f:
-            geojson_data = json.load(f)
-        print("GeoJSON data loaded successfully:")
-        print(geojson_data)
-
-        # You can access specific parts of the GeoJSON data like this:
-        if geojson_data.get('type') == 'FeatureCollection':
-            for feature in geojson_data['features']:
-                print(f"Feature properties: {feature.get('properties')}")
-                print(f"Feature geometry: {feature.get('geometry')}")
-
-        return geojson_data
-
-    except FileNotFoundError:
-        print("Error: 'my_data.geojson' not found. Please create the file or provide the correct path.")
-    except json.JSONDecodeError:
-        print("Error: Could not decode GeoJSON. The file might be malformed.")
-
-
 def load_geoframe(path):
     """
        Loads GeoJSON data from a specified file into a GeoDataFrame.
@@ -55,7 +33,7 @@ def load_geoframe(path):
 
 
 def load_ohio_state():
-    return load_geoframe(OHIO_GEOMETRY)
+    return load_geoframe('resources/ohio.geojson')
 
 
 def load_toledo():
--- ohio_concept_drift/plotter.py
+++ ohio_concept_drift/plotter.py
@@ -6,26 +6,32 @@ from ohio_concept_drift import geometry
 def plot_ohio_state():
     # Load the GeoJSON file into a GeoDataFrame
     try:
-        ohio_geometry = geometry.load_ohio_geoframe()
+        ohio_state_geometry = geometry.load_ohio_state()
+        ohio_cities_geometry = geometry.ohio_cites_geopandas()
 
         # Basic plot
-        ohio_geometry.plot(edgecolor='black', linewidth=1)
-        plt.xlabel('Longitude')
-        plt.ylabel('Latitude')
-        plt.grid(True)
-        plt.show()
+        # ohio_cities_geometry.plot(edgecolor='black', linewidth=1)
+        # plt.xlabel('Longitude')
+        # plt.ylabel('Latitude')
+        # plt.grid(True)
+        # plt.show()
 
         # Plotting different geometry types with different styles
         fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 
+        # Plit Ohio state
+        ohio_state_geometry.plot(ax=ax, color='white', edgecolor='black', legend=True)
+
+
         # Plot polygons
-        ohio_geometry[ohio_geometry.geometry.type == 'Polygon'].plot(ax=ax, color='skyblue', edgecolor='white', legend=True)
+        # ohio_cities_geometry[ohio_cities_geometry.geometry.type == 'Polygon'].plot(ax=ax, color='red', edgecolor='black', legend=True)
+        ohio_cities_geometry.plot(ax=ax, color='red', edgecolor='black', legend=True)
 
         # Plot lines
-        ohio_geometry[ohio_geometry.geometry.type == 'LineString'].plot(ax=ax, color='red', linewidth=2, legend=True)
+        # ohio_cities_geometry[ohio_cities_geometry.geometry.type == 'LineString'].plot(ax=ax, color='red', linewidth=1, legend=True)
 
         # Plot points
-        ohio_geometry[ohio_geometry.geometry.type == 'Point'].plot(ax=ax, marker='o', color='green', markersize=50, legend=True)
+        # ohio_cities_geometry[ohio_cities_geometry.geometry.type == 'Point'].plot(ax=ax, marker='o', color='green', markersize=50, legend=True)
 
         plt.title('Styled GeoJSON Plot')
         plt.xlabel('Longitude')
