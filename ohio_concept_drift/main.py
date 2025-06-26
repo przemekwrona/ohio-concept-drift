5f36a2dd666b5a11fdafdc2031382402839af16f
przemekwrona <przemekwrona25@gmail.com>
2025-06-26 11:12:01 +0200

    feat: plot ohio and avg age

--- ohio_concept_drift/main.py
+++ ohio_concept_drift/main.py
@@ -1,9 +1,16 @@
 from ohio_concept_drift import resources
+from ohio_concept_drift import geometry
 from ohio_concept_drift import plotter
+import pandas as pd
 
 
 def run():
-    plotter.plot_ohio_state()
-    # ohio_df = resources.load_ohio_arff()
-    # print(ohio_df.head())
-    print("Echo")
+    ohio_dataset = resources.load_ohio_arff()
+    ohio_dataset['ML_region'] = ohio_dataset['ML_region'].str.decode('utf-8')
+    grouped_sum = ohio_dataset.groupby('ML_region')['age'].mean()
+
+    ohio_cities = geometry.ohio_cites_geopandas()
+
+    ohio_results = pd.merge(ohio_cities, grouped_sum, on='ML_region', how='left')
+
+    plotter.plot_ohio_state(ohio_results)
