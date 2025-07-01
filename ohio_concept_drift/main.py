3ef1abc4b707d6f4c2148715bb57df181267a66d
przemekwrona <przemekwrona25@gmail.com>
2025-07-01 16:06:47 +0200

    feat: plot drift detection frequency

--- ohio_concept_drift/main.py
+++ ohio_concept_drift/main.py
@@ -7,14 +7,22 @@ import pandas as pd
 def run():
     ohio_dataset = resources.load_ohio_arff()
     ohio_dataset['ML_region'] = ohio_dataset['ML_region'].str.decode('utf-8')
+    grouped_number_of_instances_by_region = ohio_dataset.groupby('ML_region').agg(number_of_instances=('ML_region', 'count'))
+
     grouped_drift_detection_by_region = resources.load_detected_drift().groupby('region').agg(total_drift_detection=('drift_type', 'count'))
     grouped_drift_detection_by_region['total_drift_detection'] = grouped_drift_detection_by_region['total_drift_detection'].astype('Int64')
 
     ohio_cities = geometry.ohio_cites_geopandas()
 
     ohio_results = pd.merge(ohio_cities, grouped_drift_detection_by_region, left_on='ML_region', right_on='region', how='left')
-    ohio_results['total_drift_detection'] = ohio_results['total_drift_detection'].fillna(0)
+    ohio_results = pd.merge(ohio_results, grouped_number_of_instances_by_region, on='ML_region', how='left')
 
-    plotter.plot_ohio_state(ohio_results, 'number_of_detection.pdf')
+    ohio_results['total_drift_detection'] = ohio_results['total_drift_detection'].fillna(0)
+    ohio_results['number_of_instances'] = ohio_results['number_of_instances'].fillna(0)
+    ohio_results['drift_frequency_per_10k'] = 10000 * ohio_results['total_drift_detection'] / ohio_results['number_of_instances']
+
+    plotter.plot_ohio_state(ohio_results, column_name='total_drift_detection', file_name='number_of_detection.pdf', vmax=10)
+    plotter.plot_ohio_state(ohio_results, column_name='number_of_instances', file_name='number_of_instances.pdf', vmax=20000)
+    plotter.plot_ohio_state(ohio_results, column_name='drift_frequency_per_10k', file_name='drift_frequency_per_10k.pdf', vmax=5)
 
     plotter.plot_ohio_state(ohio_results)
