d61e35a969bd162422596a608d6e9a9076dcda11
przemekwrona <przemekwrona25@gmail.com>
2025-07-01 15:49:53 +0200

    feat: plot number od drifts

--- ohio_concept_drift/main.py
+++ ohio_concept_drift/main.py
@@ -7,10 +7,14 @@ import pandas as pd
 def run():
     ohio_dataset = resources.load_ohio_arff()
     ohio_dataset['ML_region'] = ohio_dataset['ML_region'].str.decode('utf-8')
-    grouped_sum = ohio_dataset.groupby('ML_region')['age'].mean()
+    grouped_drift_detection_by_region = resources.load_detected_drift().groupby('region').agg(total_drift_detection=('drift_type', 'count'))
+    grouped_drift_detection_by_region['total_drift_detection'] = grouped_drift_detection_by_region['total_drift_detection'].astype('Int64')
 
     ohio_cities = geometry.ohio_cites_geopandas()
 
-    ohio_results = pd.merge(ohio_cities, grouped_sum, on='ML_region', how='left')
+    ohio_results = pd.merge(ohio_cities, grouped_drift_detection_by_region, left_on='ML_region', right_on='region', how='left')
+    ohio_results['total_drift_detection'] = ohio_results['total_drift_detection'].fillna(0)
+
+    plotter.plot_ohio_state(ohio_results, 'number_of_detection.pdf')
 
     plotter.plot_ohio_state(ohio_results)
--- ohio_concept_drift/plotter.py
+++ ohio_concept_drift/plotter.py
@@ -4,7 +4,7 @@ from shapely.affinity import scale
 from ohio_concept_drift import geometry
 
 
-def plot_ohio_state(geodata):
+def plot_ohio_state(geodata, file_name):
     # Load the GeoJSON file into a GeoDataFrame
     try:
         ohio_state_geometry = geometry.load_ohio_state()
@@ -13,8 +13,8 @@ def plot_ohio_state(geodata):
         fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 
         # Define the scaling factor
-        scale_factor = 2.5
-        ohio_scale_factor = 1.1
+        scale_factor = 1.0
+        ohio_scale_factor = 1.0
 
         ohio_state_scaled_geometry = geodata.copy()  # Work on a copy to preserve original data
         ohio_state_scaled_geometry['geometry'] = ohio_state_scaled_geometry['geometry'].apply(
@@ -29,10 +29,10 @@ def plot_ohio_state(geodata):
 
         # Plot polygons
         # ohio_cities_geometry[ohio_cities_geometry.geometry.type == 'Polygon'].plot(ax=ax, color='red', edgecolor='black', legend=True)
-        ohio_state_scaled_geometry.plot(column='age', ax=ax, legend=True, cmap='viridis', legend_kwds={
-            'label': "Age",  # Custom label for the colorbar
+        ohio_state_scaled_geometry.plot(column='total_drift_detection', ax=ax, legend=True, cmap='viridis', vmin=0, vmax=10, legend_kwds={
+            'label': "Total Drift Detection",  # Custom label for the colorbar
             'orientation': "vertical",  # Position the colorbar horizontally
-            'shrink': 0.93,  # Reduce its size
+            'shrink': 0.88,  # Reduce its size
             'pad': 0.01,  # Padding between map and colorbar
             # 'anchor': (0.0, 0.0),  # Anchor point for positioning (x, y) relative to axes
             # 'bbox_to_anchor': (1.02, 0.5),  # Position slightly outside the right edge
@@ -58,12 +58,12 @@ def plot_ohio_state(geodata):
         ax.tick_params(axis='y', labelsize=14)
 
         # Set font size for legend text
-        ax.legend(fontsize=14)
+        # ax.legend(fontsize=14)
 
         plt.grid(True)
         plt.show()
 
-        fig.savefig("ohio.pdf", bbox_inches='tight')
+        fig.savefig(file_name, bbox_inches='tight')
 
     except FileNotFoundError:
         print("Error: 'my_plot_data.geojson' not found.")
