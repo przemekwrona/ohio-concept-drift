4d32288c625fbb32571a333bd34bc477f2911666
przemekwrona <przemekwrona25@gmail.com>
2025-11-27 12:39:03 +0100

    feat: keep results in config

---
+++ ohio_concept_drift/config.py
@@ -0,0 +1,20 @@
+usa_results_config = [
+    ('HAT_ADWIN', 'experiments/usa/output_plots_1_1/HAT_ADWIN'),
+    ('HT_ADWIN', 'experiments/usa/output_plots_1_1/HT_ADWIN'),
+    ('ARF_ADWIN', 'experiments/usa/output_plots_1_1/ARF_ADWIN'),
+    ('NB_ADWIN', 'experiments/usa/output_plots_1_1/NB_ADWIN'),
+    ('NB_HDDM_ONESIDED', 'experiments/usa/output_plots_1_1/NB_HDDM_ONESIDED'),
+    ('HT_HDDM_ONESIDED', 'experiments/usa/output_plots_1_1/HT_HDDM_ONESIDED'),
+    ('ARF_HDDM_ONESIDED', 'experiments/usa/output_plots_1_1/ARF_HDDM_ONESIDED')
+]
+
+ohio_config = [
+    ('HAT_ADWIN', 'experiments/Ohio/Eksperyment 4/HAT_ADWIN'),
+    ('HT_ADWIN', 'experiments/Ohio/Eksperyment 4/HT_ADWIN'),
+    ('ARF_ADWIN', 'experiments/Ohio/Eksperyment 4/ARF_ADWIN'),
+    ('NB_ADWIN', 'experiments/Ohio/Eksperyment 2 NB ADWIN HDDM pełne przebiegi/Eksperyment_2/NB_ADWIN'),
+    ('NB_HDDM_ONESIDED', 'experiments/Ohio/Eksperyment 2 NB ADWIN HDDM pełne przebiegi/Eksperyment_2/NB_HDDM_ONESIDED'),
+    ('SRP_HDDM_ONESIDED', 'experiments/Ohio/Eksperyment 3 SRP HDDM pełne przeliczenie/SRP_HDDM_ONESIDED'),
+    ('HT_HDDM_ONESIDED', 'experiments/Ohio/Eksperyment 4/HT_HDDM_ONESIDED'),
+    ('ARF_HDDM_ONESIDED', 'experiments/Ohio/ARF_HDDM_ONESIDED')
+]
