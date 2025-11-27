a764ea75f90444a8b79cd7c048f1242a00e8a15f
przemekwrona <przemekwrona25@gmail.com>
2025-11-27 12:39:15 +0100

    feat: dictionary for usa states

--- ohio_concept_drift/usa/dictionary.py
+++ ohio_concept_drift/usa/dictionary.py
@@ -50,3 +50,67 @@ USA_STATES_CODES = {
     "WI": "WI Wisconsin",
     "WY": "WY Wyoming"
 }
+
+UNKNOWN = "UNKNOWN"
+CAR = "CAR"
+PT = "PT"
+BIKE = "BIKE"
+WALK = "WALK"
+OTHER = "OTHER"
+
+USA_TMC_CODES_2009 = {
+    "-7": OTHER,  # I prefer not to answer / Refused
+    "-8": OTHER,  # I don't know
+    "-9": OTHER,  # Not ascertained
+    "1": CAR,  # Car
+    "2": CAR,  # Van
+    "3": CAR,  # SUV
+    "4": CAR,  # Pick-up Truck
+    "5": CAR,  # Other Truck
+    "6": CAR,  # RV
+    "7": CAR,  # Motorcycle
+    "8": CAR,  # Light Electric Vehicle
+    "9": PT,  # Local Public Bus
+    "10": PT,  # Commuter Bus
+    "11": PT,  # School bus
+    "12": OTHER,  # Charter/Tour
+    "13": PT,  # City-to-City Bus
+    "14": PT,  # Shuttle Bus
+    "15": PT,  # Amtrak/Inter-city Train
+    "16": PT,  # Commuter Train
+    "17": PT,  # Subway/elevated Train
+    "18": PT,  # Streetcar/Trolley
+    "19": PT,  # Taxicab
+    "20": OTHER,  # Ferry
+    "21": OTHER,  # Airplane
+    "22": BIKE,  # Bicycle
+    "23": WALK,  # Walk
+    "97": OTHER,  # Other
+}
+
+USA_TMC_CODES_2017 = {
+    "-7": OTHER,  # I prefer not to answer / Refused
+    "-8": OTHER,  # I don't know
+    "-9": OTHER,  # Not ascertained
+    "1": WALK,  # Walk
+    "2": BIKE,  # Bicycle
+    "3": CAR,  # Car
+    "4": CAR,  # SUV
+    "5": CAR,  # Van (Minivan)
+    "6": CAR,  # Pickup Truck
+    "7": CAR,  # Golf cart/Segway
+    "8": CAR,  # Motorcycle/Moped
+    "9": CAR,  # RV (motorhome, ATV, Snowmobile)
+    "10": PT,  # School bus
+    "11": PT,  # Public or Commuter Bus
+    "12": OTHER,  # Paratransit/Dial-a-Ride
+    "13": PT,  # Private/Charter/Tour/Shuttle Bus
+    "14": PT,  # City-to-City Bus (Greyhound, Megabus)
+    "15": PT,  # Amtrak/Commuter Rail
+    "16": PT,  # Subway/Elevated/Light Rail/Streetcar
+    "17": PT,  # Taxi/Limo (including Uber/Lyft)
+    "18": PT,  # Rental Car (Inc. Zipcar and Car2Go)
+    "19": OTHER,  # Airplane
+    "20": OTHER,  # Boat/Ferry/Water Taxi
+    "97": OTHER,  # Other
+}
