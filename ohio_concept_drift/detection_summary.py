2f3adb68f2507988dfe8a8e7e0409b376d925b72
przemekwrona <przemekwrona25@gmail.com>
2025-11-27 13:47:11 +0100

    feat: summary nuber of detection

---
+++ ohio_concept_drift/detection_summary.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+
+from ohio_concept_drift.ohio_data import load_usa_data_per_experiment, load_ohio_data_per_experiment
+from ohio_concept_drift.usa.dictionary import USA_STATES_CODES
+
+
+def summary_ohio_number_of_detection(results_config, target_directory, column_name='total_drift_detection'):
+    results = [(experiment, load_ohio_data_per_experiment(experiment_name=experiment, drift_results_directory=drift_path, column_name=column_name)) for
+               experiment, drift_path in results_config]
+
+    summary_number_of_detection(results, target_directory, column_name='total_drift_detection')
+
+
+def summary_usa_number_of_detection(results_config, target_directory, column_name='total_drift_detection'):
+    results = [(experiment, load_usa_data_per_experiment(experiment_name=experiment, drift_results_file=drift_path, column_name=column_name)) for
+               experiment, drift_path in results_config]
+
+    summary_number_of_detection(results, target_directory, column_name='total_drift_detection')
+
+
+def summary_number_of_detection(results_config, target_directory, column_name='total_drift_detection'):
+    results = pd.DataFrame(data={})
+
+    for (experiment, drift_results) in results_config:
+        print(experiment)
+
+        drift_results.loc['Total'] = drift_results.sum()
+
+        experiment_column = experiment
+        total_column = f'{experiment}_total'
+        _10k_column = f'{experiment}_10k'
+        drift_results[_10k_column] = np.where(drift_results[total_column] > 0, 10_000 * drift_results[experiment_column] / drift_results[total_column], 0)
+
+        drift_results = drift_results[['ML_region', experiment_column, _10k_column]]
+
+        if results.empty:
+            results = drift_results
+        else:
+            results = pd.merge(results, drift_results, on='ML_region')
+
+    results['ML_region'] = results['ML_region'].map(USA_STATES_CODES).str.upper()
+
+    with open(f'{target_directory}/{column_name}_tab_summary_v2.tex', "w") as f:
+        f.write(build_latex(results))
+
+    print('DONE')
+
+
+def build_latex(results):
+    latex_code = results.to_latex(
+        index=False,
+        # caption="The aggregate number of concept drift instances identified using the designated algorithm.",
+        # label=f'tab:{column_name}',
+        float_format="%.2f"  # Format floats to 2 decimal places
+    )
+
+    latex_code = latex_code.replace('\\toprule', '\hline')
+    latex_code = latex_code.replace('\\midrule', '\hline')
+    latex_code = latex_code.replace('\\bottomrule', '\hline')
+
+    latex_code = latex_code.replace('& 0 &', '& \\textbf{0} &')
+    latex_code = latex_code.replace('& 0.00 &', '& \\textbf{0.00} &')
+    latex_code = latex_code.replace('& 0.00 \\', '& \\textbf{0.00} \\')
+    latex_code = latex_code.replace('ML_region', 'Region')
+    latex_code = latex_code.replace('HAT_ADWIN & HAT_ADWIN_10k', '\multicolumn{2}{c|}{\makecell{HAT \\\\ ADWIN}}')
+    latex_code = latex_code.replace('HT_ADWIN & HT_ADWIN_10k', '\multicolumn{2}{c|}{\makecell{HT \\\\ ADWIN}}')
+    latex_code = latex_code.replace('ARF_ADWIN & ARF_ADWIN_10k', '\multicolumn{2}{c|}{\makecell{ARF \\\\ ADWIN}}')
+    latex_code = latex_code.replace('NB_ADWIN & NB_ADWIN_10k', '\multicolumn{2}{c|}{\makecell{NB \\\\ ADWIN}}')
+    latex_code = latex_code.replace('NB_HDDM_ONESIDED & NB_HDDM_ONESIDED_10k', '\multicolumn{2}{c|}{\makecell{NB HDDM \\\\ ONESIDED}}')
+    latex_code = latex_code.replace('SRP_HDDM_ONESIDED & SRP_HDDM_ONESIDED_10k', '\multicolumn{2}{c|}{\makecell{SRP HDDM \\\\ ONESIDED}}')
+    latex_code = latex_code.replace('HT_HDDM_ONESIDED & HT_HDDM_ONESIDED_10k', '\multicolumn{2}{c|}{\makecell{HT HDDM \\\\ ONESIDED}}')
+    latex_code = latex_code.replace('ARF_HDDM_ONESIDED & ARF_HDDM_ONESIDED_10k', '\multicolumn{2}{c|}{\makecell{ARF HDDM \\\\ ONESIDED}}')
+
+    return latex_code
